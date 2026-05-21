352a054f959ff7b0e2789bfa7c1c4572da59506e
allianzgruppedarkbulls <allianzgruppe.darkbulls@gmail.com>
2026-05-21 13:38:01 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Diese Datei kannst du in GitHub Pages sicher veröffentlichen.
 window.BULLS_CONFIG = {
   // Google Apps Script WebApp-URL (als API für Login/Chat/Admin)
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyqi6-hTKn0f9rQvG829aZKn0qVPhiEmac1Jf3zhCImcp78FCTinD7kLuq1l_PpaWzc9g/exec",
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzYGf7F3gShmZQHiRCyXpdYiZzi-xO7HE7kyavtCi9KdMe13LkiwDZ6nRCMxwddlpeQ0w/exec",
 };
